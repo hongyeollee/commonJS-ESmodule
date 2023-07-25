f280e7f174ec42f6b869ab57cf1d9542ed6d6322
이홍열 <zeler1004@gmail.com>
2023-07-25 16:46:21 +0900

    babel-ESmodule 로그아웃 기능 구현 - accessToken, refreshToken 모두 쿠키를 사용하도록 하고 쿠키삭제로 로그아웃 기능 구현

--- ESmodule-on-babel/routes/userRouter.js
+++ ESmodule-on-babel/routes/userRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import * as userController from "../controllers/userController.js";
 
 router.post("/login", userController.login);
+router.post("/logout", userController.logout);
 
 export default router;
