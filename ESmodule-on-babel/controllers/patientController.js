f1ce907c54e603bf3586f4863a050785540bb887
이홍열 <zeler1004@gmail.com>
2023-08-02 17:36:21 +0900

    babel-ESmodule 이미지업로드 기능개선 - 이미지업로드 코드 모듈화

--- ESmodule-on-babel/controllers/patientController.js
+++ ESmodule-on-babel/controllers/patientController.js
@@ -13,28 +13,32 @@ export const createPatient = catchAsync(async (req, res) => {
   if (!name || !ssn || !birthDate || !cellPhone || !address1) {
     const error = new Error("KEY_ERROR");
     error.statusCode = 400;
+    deleteFile(image.path);
     throw error;
   }
+  try {
+    const data = await patientService.createPatient(
+      name,
+      ssn,
+      birthDate,
+      cellPhone,
+      phone,
+      email,
+      address1,
+      address2,
+      image
+    );
 
-  const data = await patientService.createPatient(
-    name,
-    ssn,
-    birthDate,
-    cellPhone,
-    phone,
-    email,
-    address1,
-    address2,
-    image
-  );
-
-  return res.status(200).json({ code: 200, message: "Success", data });
+    return res.status(200).json({ code: 200, message: "Success", data });
+  } catch (err) {
+    deleteFile(image.path);
+    console.error(err);
+    throw err;
+  }
 });
 
 export const deletePatient = catchAsync(async (req, res) => {
   const { patientId } = req.params;
-  const [getImageInfoByPatientId] =
-    await patientService.getImageInfoByPatientId(patientId);
   const existPatientInfo = await patientService.getIdByPatientId(patientId);
 
   if (!existPatientInfo) {
@@ -42,19 +46,8 @@ export const deletePatient = catchAsync(async (req, res) => {
     error.statusCode = 400;
     throw error;
   }
-  const filename = await getImageInfoByPatientId.imageUrl;
-  if (filename) {
-    fs.access(filename, fs.constants.F_OK, (err) => {
-      if (err) {
-        console.error(err);
-        throw err;
-      } else {
-        deleteFile(filename);
-      }
-    });
-  }
-
-  await patientService.deletePatient(patientId, filename);
+  deletePatientImage(patientId);
+  await patientService.deletePatient(patientId);
 
   return res.status(200).json({ code: 200, message: "Success" });
 });
@@ -84,21 +77,47 @@ export const updatePatient = catchAsync(async (req, res) => {
   if (!existPatientInfo) {
     const error = new Error("NOT_EXIST_PATIENT");
     error.statusCode = 400;
+    if (image) {
+      deleteFile(image.path);
+    }
     throw error;
   }
+  if (image) deletePatientImage(patientId);
+  try {
+    const data = await patientService.updatePatient(
+      name,
+      ssn,
+      birthDate,
+      cellPhone,
+      phone,
+      email,
+      address1,
+      address2,
+      image,
+      patientId
+    );
 
-  const data = await patientService.updatePatient(
-    name,
-    ssn,
-    birthDate,
-    cellPhone,
-    phone,
-    email,
-    address1,
-    address2,
-    image,
-    patientId
-  );
-
-  return res.status(200).json({ code: 200, message: "Success", data });
+    return res.status(200).json({ code: 200, message: "Success", data });
+  } catch (err) {
+    deleteFile(image.path);
+    console.error(err);
+    throw err;
+  }
 });
+
+const deletePatientImage = async (patientId) => {
+  const [getImageInfoByPatientId] =
+    await patientService.getImageInfoByPatientId(patientId);
+
+  const filename = await getImageInfoByPatientId.imageUrl;
+  if (filename) {
+    fs.access(filename, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.error(err);
+        throw err;
+      } else {
+        deleteFile(filename);
+      }
+    });
+  }
+};
--- ESmodule-on-babel/middlsware/imageUploader.js
+++ ESmodule-on-babel/middlsware/imageUploader.js
@@ -3,20 +3,19 @@ import path from "path";
 import fs from "fs";
 
 //업로더의 이미지를 로컬서버에 저장 로직
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(
-      null,
-      "/Users/hongyeol/Desktop/honyeollee/commonJS-ESmodule/ESmodule-on-babel/imageStorage"
-    );
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().valueOf() + path.extname(file.originalname));
-    //cb(null, file.fieldname + "-" + Date.now()); 이 코드로하면 이미지파일이 등록되지 않는 현상 발생, 추가적인 공부필요.
-  },
-});
+export const createImageUploader = (destination) => {
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, destination);
+    },
+    filename: (req, file, cb) => {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+      //cb(null, file.fieldname + "-" + Date.now()); 이 코드로하면 이미지파일이 등록되지 않는 현상 발생, 추가적인 공부필요.
+    },
+  });
 
-export const upload = multer({ storage });
+  return multer({ storage });
+};
 
 //파일 삭제를 위한 로직
 export const deleteFile = (filename) => {
